0d6a3749694b9ca75ed3767424709519bd629647
Mariano Nieves <mariano.nieves@gmail.com>
2022-07-01 16:59:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const SongsObject = require('./songs'); 
 const  {TwitterApi} = require('twitter-api-v2');
+
 let client = null;
 let postEnabled = false;
 
 module.exports = function (context, req) {
     context.log('>>>> Twitter test processed a request.');
+
+    context.log('>>>> process.env.twitter_consumer_key :: ' + process.env.twitter_consumer_secret);
+    context.log('>>>> process.env.twitter_consumer_secret :: ' + process.env.twitter_consumer_key);
+    context.log('>>>> process.env.twitter_access_token :: ' + process.env.twitter_access_token);
+    context.log('>>>> process.env.twitter_access_token_secret :: ' + process.env.twitter_access_token_secret);
+
+    songs = new SongsObject();
+    context.log('>>>> - - - - - - - - ' );
+ 
 
     client = new TwitterApi({
         appKey: process.env.twitter_consumer_key,
@@ -12,9 +23,11 @@ module.exports = function (context, req) {
         accessSecret: process.env.twitter_access_token_secret,
     });
 
+    var messageToTweet = songs.getRandomLyric();
+
     context.log('>>>> hace el post ' );
     if( postEnabled ){
-        client.v1.tweet('This tweet was written by a bot').then((val) => {
+        client.v1.tweet( messageToTweet ).then((val) => {
 
             context.log(">>>> val" + val);
             context.log(">>>> success");
@@ -31,11 +44,14 @@ module.exports = function (context, req) {
                 status: 400,
                 body: err
             };
-            context.done();
-
+            context.done();            
         })
+    } else {  
+        context.res = {
+            status: 200,
+            body: ">>>> the post tweet feature is DISABLED in the app, but it would tweet : " + messageToTweet
+        };
+        context.done();
     }
-
-
 };
 
